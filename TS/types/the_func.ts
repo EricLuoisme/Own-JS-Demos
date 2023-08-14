9cda463e27bcc2460e9bfda7bb2ed540db0a9c38
roylic <lykismekk@outlook.com>
2023-08-14 23:52:16 +0800

    add simple func & array stuff

--- TS/types/the_func.ts
+++ TS/types/the_func.ts
@@ -10,5 +10,27 @@ let tom: IPerson = {
     id: 1,
     name: 'Tom'
 }
-
 console.log(tom)
+
+
+// array
+let fibonacci: Array<number> = [1, 2, 3];
+fibonacci.push(6);
+
+// func
+let mySum: (x: number, y: number) => number = function (x: number, y: number): number {
+    return x + y;
+}
+
+// func interface
+interface SearchFunc {
+    (source: string, subString: string, extra?: string): boolean;
+}
+
+// is some interface
+let mySearch: SearchFunc;
+
+// than implement
+mySearch = function (source: string, subString: string) {
+    return source.search(subString) !== -1;
+}
