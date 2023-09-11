06d2c892711b04003a9bb9d030d72109d060742b
roylic <lykismekk@outlook.com>
2023-09-11 23:31:41 +0800

    add simple error stuff on bun

--- BunDemo/bun_http_server.jsx
+++ BunDemo/bun_http_server.jsx
@@ -1,8 +1,16 @@
 const server = Bun.serve({
     port: 3030,
     fetch(request) {
-        return new Response("Hello, Bun!");
+        // return new Response("Hello, Bun!");
+        throw new Error("woops!")
     },
+    error(error) {
+        return new Response(`<pre>${error}\n${error.stack}</pre>`, {
+            headers: {
+                "Content-Type": "text/html"
+            }
+        })
+    }
 });
 
 console.log(`Listening on localhost:${server.port}`)
