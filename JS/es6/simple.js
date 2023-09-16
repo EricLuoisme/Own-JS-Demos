a94d54032facdf72e8dfeeb75d4129e980e69e06
roylic <lykismekk@outlook.com>
2023-09-16 13:36:05 +0800

    simple replace

--- JS/es6/simple.js
+++ JS/es6/simple.js
@@ -1,8 +1,18 @@
-for(let i = 100; i < 3; i++) {
-    console.log(i)
-}
+// for(let i = 100; i < 3; i++) {
+//     console.log(i)
+// }
 
-var [foo, [[bar], baz]] = [1, [[2], 3]];
-console.log(foo)
-console.log(bar)
-console.log(baz)
+// var [foo, [[bar], baz]] = [1, [[2], 3]];
+// console.log(foo)
+// console.log(bar)
+// console.log(baz)
+
+let x = 1;
+let y = 2;
+console.log(x);
+console.log(y);
+
+// can use this to swap value of the variable
+[x, y] = [y, x];
+console.log(x);
+console.log(y);
